b08c6f8c7251bd7dd547695bb073ab526573d77b
Isabel Taswell <itaswell@hamilton.edu>
2016-08-05 13:53:58 -0400

    Began custom calendar view.

---
+++ Holy Cow/HolyCalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  HolyCalendarView.swift
+//  Holy Cow
+//
+//  Created by Isabel Taswell on 8/2/16.
+//  Copyright © 2016 Tobin Bell. All rights reserved.
+//
+
+import UIKit
+
+class HolyCalendarView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
